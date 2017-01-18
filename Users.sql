27e28fdaf9ed3854cd65c2dfbe36403c7ea0eb58
jeremyjohnson7 <jeremyjohnson7@mail.weber.edu>
2017-01-18 13:06:48 -0700

    Created project

---
+++ Users.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [UserId] NCHAR(10) NOT NULL, 
+    [firstName] VARCHAR(50) NOT NULL, 
+    [lastName] VARCHAR(50) NOT NULL, 
+    [password] VARCHAR(50) NOT NULL, 
+    [age] INT NULL, 
+    [lastUpdate] DATETIME NOT NULL
+)
